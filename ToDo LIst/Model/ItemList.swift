192914b760c40ec2d1094818e017635b078dbaf2
fares elsokary <fareselsokary@faress-mac.local>
2018-09-25 11:24:44 +0200

    create MVC Model

---
+++ ToDo LIst/Model/ItemList.swift
@@ -0,0 +1,14 @@
+//
+//  ItemList.swift
+//  ToDo LIst
+//
+//  Created by fares elsokary on 9/25/18.
+//  Copyright © 2018 fares elsokary. All rights reserved.
+//
+
+import Foundation
+class ItemList{
+    var title : String = ""
+    var done : Bool = false
+
+}
